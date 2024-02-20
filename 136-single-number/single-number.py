742d7d92e908f07ff0c648802150f9ef774bbd61
Mohammed saleek <mohammedsaleek123@gmail.com>
2024-02-20 12:26:26 +0530

    Time: 106 ms (42.42%) | Memory: 15.2 MB (48.01%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        mapper = {}
+        for index, nums in enumerate(nums):
+            if nums in mapper:
+                mapper[nums] += 1
+            elif nums not in mapper:
+                mapper[nums] = 1
+        for key,value in mapper.items():
+            if value == 1:
+                unique_number = key
+        # unique_number = next((key for key,value in mapper.items() if value == 1), None)
+        return unique_number
