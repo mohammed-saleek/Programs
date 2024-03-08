6306c61c1fb576911a9983ca42d00a21812a9fa8
Mohammed saleek <mohammedsaleek123@gmail.com>
2024-03-08 23:01:38 +0530

    Time: 45 ms (12.01%) | Memory: 11.8 MB (46.84%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -11,22 +11,6 @@ class Solution(object):
         #         unique_list.append(number)
         # response = list(set(unique_list))
         # return response
-        # unique_list = set([x for x in nums1 if x in nums2])
-        # return unique_list
-        # taking two hashmaps to store the each value and its count
-        hashMap1 = defaultdict(int)
-        hashMap2 = defaultdict(int)
-        # a list to store the value
-        answerList = []
-
-        for i in nums1:
-            hashMap1[i] = hashMap1[i] + 1
-
-        for i in nums2:
-            hashMap2[i] = hashMap2[i] + 1
-
-        # for each value in hashMap1 , if that is present in hashMap2, we append it to the answerList
-        for key in hashMap1:
-            if key in hashMap2:
-                answerList.append(key)
-        return answerList
+        unique_list = set([x for x in nums1 if x in nums2])
+        return unique_list
+        
