00fd86c329544162ab9df94d98f71e5912083f68
Mohammed saleek <mohammedsaleek123@gmail.com>
2024-05-20 21:36:21 +0530

    Time: 59 ms (23.29%) | Memory: 12 MB (45.48%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.py
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -4,9 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        # result = []
+        # for i in range(2):
+        #     for index in range(len(nums)):
+        #         result.append(nums[index])
+        # return result
         result = []
-        for i in range(2):
-            for index in range(len(nums)):
-                result.append(nums[index])
+        l2 = []
+        for index in range(len(nums)):
+            result.append(nums[index])
+            l2.append(nums[index])
+        result.extend(l2)
         return result
         
